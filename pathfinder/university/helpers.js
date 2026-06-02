6aa926654d642b443ced425b8ade4d46196ec077
Craig <craigwpendleton@gmail.com>
2026-06-02 12:51:41 -0400

    Phase 1: Create pluggable architecture - router + framework + pathfinder structure
    - Root index.html: Router that redirects to pathfinder based on URL hash
    - pathfinder/university/index.html: Full HTML page with framework code (renamed from framework.js)
    - pathfinder/university/renderers.js: Stub for rendering functions (to be populated)
    - pathfinder/university/helpers.js: Stub for helper functions (to be populated)
    - pathfinder/university/config.js: Already exists with configuration
    
    All rendering and helper functions currently stay in index.html; incrementally move to renderers.js and helpers.js

---
+++ pathfinder/university/helpers.js
@@ -0,0 +1,8 @@
+// University Pathfinder - Helper Functions
+// All get*, format*, and other helper functions for the university pathfinder
+
+const universityHelpers = {
+  // Stubs for now - helper functions will be moved here incrementally
+};
+
+window.universityHelpers = universityHelpers;
---
+++ pathfinder/university/renderers.js
@@ -0,0 +1,8 @@
+// University Pathfinder - Rendering Functions
+// All render* functions for the university pathfinder
+
+const universityRenderers = {
+  // Stubs for now - rendering functions will be moved here incrementally
+};
+
+window.universityRenderers = universityRenderers;
